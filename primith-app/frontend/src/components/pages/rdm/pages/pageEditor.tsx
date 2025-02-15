5da6978e10f0b335389863800ec4b215259c7e6a
Michael Forbes <mforbes5223@hotmail.com>
2025-02-15 00:30:15 -0600

    updated editor table edit icons

--- primith-app/frontend/src/components/pages/rdm/pages/pageEditor.tsx
+++ primith-app/frontend/src/components/pages/rdm/pages/pageEditor.tsx
@@ -27,6 +27,7 @@ import {
   Redo2,
   Bold,
   Italic,
+  Combine,
   Underline as UnderlineIcon,
   Strikethrough,
   AlignLeft,
@@ -147,84 +148,108 @@ const TableOptions: React.FC<TableOptionsProps> = ({ editor }) => {
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().addColumnBefore().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"  viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M13,2A2,2 0 0,0 11,4V20A2,2 0 0,0 13,22H22V2H13M20,10V14H13V10H20M20,16V20H13V16H20M20,4V8H13V4H20M9,11H6V8H4V11H1V13H4V16H6V13H9V11Z" />
+        </svg>
           <span>Column Before</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().addColumnAfter().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"  viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M11,2A2,2 0 0,1 13,4V20A2,2 0 0,1 11,22H2V2H11M4,10V14H11V10H4M4,16V20H11V16H4M4,4V8H11V4H4M15,11H18V8H20V11H23V13H20V16H18V13H15V11Z" />
+        </svg>
           <span>Column After</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().deleteColumn().run()}
         >
-          <Trash2 className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"  viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M4,2H11A2,2 0 0,1 13,4V20A2,2 0 0,1 11,22H4A2,2 0 0,1 2,20V4A2,2 0 0,1 4,2M4,10V14H11V10H4M4,16V20H11V16H4M4,4V8H11V4H4M17.59,12L15,9.41L16.41,8L19,10.59L21.59,8L23,9.41L20.41,12L23,14.59L21.59,16L19,13.41L16.41,16L15,14.59L17.59,12Z" />
+        </svg>
           <span>Delete Column</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().addRowBefore().run()}
         >
-          <Plus className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"  viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M22,14A2,2 0 0,0 20,12H4A2,2 0 0,0 2,14V21H4V19H8V21H10V19H14V21H16V19H20V21H22V14M4,14H8V17H4V14M10,14H14V17H10V14M20,14V17H16V14H20M11,10H13V7H16V5H13V2H11V5H8V7H11V10Z" />
+        </svg>
           <span>Row Before</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().addRowAfter().run()}
         >
-          <Plus className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"  viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M22,10A2,2 0 0,1 20,12H4A2,2 0 0,1 2,10V3H4V5H8V3H10V5H14V3H16V5H20V3H22V10M4,10H8V7H4V10M10,10H14V7H10V10M20,10V7H16V10H20M11,14H13V17H16V19H13V22H11V19H8V17H11V14Z" />
+        </svg>
           <span>Row After</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().deleteRow().run()}
         >
-          <Trash2 className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"  viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M9.41,13L12,15.59L14.59,13L16,14.41L13.41,17L16,19.59L14.59,21L12,18.41L9.41,21L8,19.59L10.59,17L8,14.41L9.41,13M22,9A2,2 0 0,1 20,11H4A2,2 0 0,1 2,9V6A2,2 0 0,1 4,4H20A2,2 0 0,1 22,6V9M4,9H8V6H4V9M10,9H14V6H10V9M16,9H20V6H16V9Z" />
+        </svg>
           <span>Delete Row</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().mergeCells().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+          <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#5f6368">
+            <path d="M120-120v-240h80v160h160v80H120Zm480 0v-80h160v-160h80v240H600ZM287-327l-57-56 57-57H80v-80h207l-57-57 57-56 153 153-153 153Zm386 0L520-480l153-153 57 56-57 57h207v80H673l57 57-57 56ZM120-600v-240h240v80H200v160h-80Zm640 0v-160H600v-80h240v240h-80Z"/>
+          </svg>
           <span>Merge Cells</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().splitCell().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M19 14H21V20H3V14H5V18H19V14M3 4V10H5V6H19V10H21V4H3M11 11V13H8V15L5 12L8 9V11H11M16 11V9L19 12L16 15V13H13V11H16Z" />
+        </svg>
           <span>Split Cells</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().toggleHeaderColumn().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M4 3H18C19.11 3 20 3.9 20 5V12.08C18.45 11.82 16.92 12.18 15.68 13H12V17H13.08C12.97 17.68 12.97 18.35 13.08 19H4C2.9 19 2 18.11 2 17V5C2 3.9 2.9 3 4 3M4 7V11H10V7H4M12 7V11H18V7H12M4 13V17H10V13H4M15.94 18.5H17.94V14.5H19.94V18.5H21.94L18.94 21.5L15.94 18.5" />
+        </svg>
           <span>Header Column</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().toggleHeaderRow().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M4 3H18C19.11 3 20 3.9 20 5V12.08C18.45 11.82 16.92 12.18 15.68 13H12V17H13.08C12.97 17.68 12.97 18.35 13.08 19H4C2.9 19 2 18.11 2 17V5C2 3.9 2.9 3 4 3M4 7V11H10V7H4M12 7V11H18V7H12M4 13V17H10V13H4M19.44 21V19H15.44V17H19.44V15L22.44 18L19.44 21" />
+        </svg>
           <span>Header Row</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().toggleHeaderCell().run()}
         >
-          <TableIcon className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M4 3H18C19.11 3 20 3.9 20 5V12.08C18.45 11.82 16.92 12.18 15.68 13H12V17H13.08C12.97 17.68 12.97 18.35 13.08 19H4C2.9 19 2 18.11 2 17V5C2 3.9 2.9 3 4 3M4 7V11H10V7H4M12 7V11H18V7H12M4 13V17H10V13H4M17.75 21L15 18L16.16 16.84L17.75 18.43L21.34 14.84L22.5 16.25L17.75 21" />
+        </svg>
           <span>Header Cell</span>
         </li>
         <li
           className="flex items-center px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
           onClick={() => editor.chain().focus().deleteTable().run()}
         >
-          <Trash2 className="w-4 h-4 mr-2" />
+        <svg className="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="#5f6368">
+        <path d="M15.46,15.88L16.88,14.46L19,16.59L21.12,14.46L22.54,15.88L20.41,18L22.54,20.12L21.12,21.54L19,19.41L16.88,21.54L15.46,20.12L17.59,18L15.46,15.88M4,3H18A2,2 0 0,1 20,5V12.08C18.45,11.82 16.92,12.18 15.68,13H12V17H13.08C12.97,17.68 12.97,18.35 13.08,19H4A2,2 0 0,1 2,17V5A2,2 0 0,1 4,3M4,7V11H10V7H4M12,7V11H18V7H12M4,13V17H10V13H4Z" />
+        </svg>
           <span>Delete Table</span>
         </li>
       </ul>
@@ -319,7 +344,7 @@ const PageEditor = ({
     if (!editor) return null;
   
     return (
-      <div className="absolute top-full right-0 bg-white border border-gray-200 rounded-lg shadow-lg p-2 min-w-[175px] z-50">
+      <div className="absolute top-full right-0 bg-white border border-gray-200 rounded-lg shadow-lg p-2 min-w-[150px] z-50">
         <ul className="space-y-1">
           <li
             className="flex items-center px-3 py-1 text-sm text-gray-700 hover:bg-gray-100 rounded cursor-pointer"
